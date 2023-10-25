9587e220464ac77106d926cdf7ecfd0c6ec8ec60
Erlich Sefi <erlichsefi@gmail.com>
2023-10-25 20:53:12 +0300

    Update get_url.js

--- extension/shared/extract/get_url.js
+++ extension/shared/extract/get_url.js
@@ -1,6 +1,6 @@
 export default function get_url(){
     const response = window.location.href.substring(0, 60)
     console.log("url is= "+response);
-    return {"url":response}
+    return response
 }
 //console.log(get_url())
