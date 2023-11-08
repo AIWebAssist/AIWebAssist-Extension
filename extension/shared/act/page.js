7b1815fb6830c7ca6a0f0cdd8b5e125dec28624e
Erlich Sefi <erlichsefi@gmail.com>
2023-11-08 17:10:14 +0000

    /

--- extension/shared/act/page.js
+++ extension/shared/act/page.js
@@ -1,6 +1,5 @@
 function refresh(){
     location.reload();
-    return true,"";
 }
 
 function go_back(){
@@ -9,20 +8,18 @@ function go_back(){
     }catch (error){
         return "failed to back "+error
     }
-    return true,"";
 }
 
 function move_to_url(url){
     if (!url.startsWith("http://") && !url.startsWith("https://")){
-        return false,"you should provide a url with http or https prefix."
+        return "you should provide a url with http or https prefix."
     }
 
     window.location.href = url;
 
     if (window.location.href !== url){
-        return false,"new url is not "+url;
+        return "new url is not "+url;
     }
-    return true,"";
 }
 
 export {move_to_url,go_back,refresh};
