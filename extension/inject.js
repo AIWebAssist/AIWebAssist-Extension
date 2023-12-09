f11a50148fbac4248d435adf5b5c76fe5ee6ce1d
Erlich Sefi <erlichsefi@gmail.com>
2023-12-09 14:11:03 +0200

    Update inject.js

--- extension/inject.js
+++ extension/inject.js
@@ -1,7 +1,8 @@
 (async () => {
     // Check if the circle already exists
     if (!document.getElementById('ai-assistance-circle')) {
-    
+        // the popup to manage 
+        let popup = null;
         const session_id = await new Promise((resolve) => {
             chrome.runtime.sendMessage({ contentScriptQuery: "get_session_id" }, function(response) {
                 resolve(response);
@@ -14,11 +15,11 @@
         }
 
         // Function to get storage for a specific tab
-        async function getTabStorage(session_id, keyword) {
+        async function getTabStorage(session_id, keyword, defaultValue) {
             return await new Promise((resolve) => {
                 var tabStorageKey = getTabStorageKey(session_id, keyword);
                 chrome.storage.local.get(tabStorageKey, function(result) {
-                    resolve(result[tabStorageKey]);
+                    resolve(result[tabStorageKey] !== undefined ? result[tabStorageKey] : defaultValue);
                 });
             });
         }
@@ -30,88 +31,9 @@
             storageData[tabStorageKey] = value;
             chrome.storage.local.set(storageData);
         }
-    
-        const src = chrome.runtime.getURL('./content.js');
-        const contentScript = await import(src);
-    
-        // Step 1: Create the round circle element
-        const circle = document.createElement('div');
-        circle.id = 'ai-assistance-circle';
-        // Add an image icon inside the circle
-        const icon = document.createElement('img');
-        icon.src = chrome.runtime.getURL('cricle.png'); // Update with the correct path to your 128x128 icon
-        icon.style.width = '100%';
-        icon.style.height = '100%';
-        
-        icon.style.clipPath = 'circle(50% at center)';
-        icon.style.webkitClipPath = 'circle(50% at center)';
-    
-        circle.appendChild(icon);
-    
-    
-        // <--- function to make sure it draggable --->
-        // Additional code for making the circle draggable
-        let isDragging = false;
-        let offsetX, offsetY;
-    
-        // Function to handle the start of dragging
-        function startDrag(e) {
-            isDragging = true;
-            offsetX = e.clientX - circle.getBoundingClientRect().left;
-            offsetY = e.clientY - circle.getBoundingClientRect().top;
-        }
-    
-        // Function to handle the dragging movement
-        function dragMove(e) {
-            if (isDragging) {
-                const x = e.clientX - offsetX;
-                const y = e.clientY - offsetY;
-    
-                circle.style.left = `${x}px`;
-                circle.style.top = `${y}px`;
-            }
-        }
-    
-        // Function to handle the end of dragging
-        function endDrag() {
-            isDragging = false;
-        }
-    
-        // Add event listeners for dragging
-        circle.addEventListener('mousedown', startDrag);
-        document.addEventListener('mousemove', dragMove);
-        document.addEventListener('mouseup', endDrag);
-    
-    
-    
-        // Step 2: Apply CSS styles directly in JavaScript
-        circle.style.width = '50px';
-        circle.style.height = '50px';
-        circle.style.backgroundColor = '#3498db';
-        circle.style.borderRadius = '50%';
-        circle.style.position = 'fixed';
-        circle.style.bottom = '20px';
-        circle.style.right = '20px';
-        circle.style.display = 'flex';
-        circle.style.alignItems = 'center';
-        circle.style.justifyContent = 'center';
-        circle.style.cursor = 'pointer';
-        circle.style.color = 'white';
-        circle.style.fontWeight = 'bold';
-        circle.style.zIndex = '2147483646';
 
-        let popup = null; // Variable to store the pop-up element
-    
-        // Step 3: Add event listener to the circle element
-        circle.addEventListener('click', async function () {
-            if (popup) {
-                // If pop-up is already open, close it by removing it from the DOM
-                document.body.removeChild(popup);
-                popup = null; // Reset the variable
-            } else {
-                // <--- function to make sure the popmenu doesn't blocked --->
-                // Calculate position for the popupContent near the circle
-                const circleRect = circle.getBoundingClientRect();
+        async function pop_menu(){
+            const circleRect = circle.getBoundingClientRect();
                 const popupWidth = 300; // Adjust this width as needed
                 const offsetHeight = 100;
                 const popupX = Math.min(circleRect.right + 10, window.innerWidth - popupWidth - 40);
@@ -151,7 +73,7 @@
                 const active = document.getElementById("myCheckbox");
                 const errorEl = document.getElementById("error");
                 const checkboxText = document.getElementById("checkboxText");
-                const closeButton = popup.querySelector('#closeButton');
+                const closeButton = document.querySelector('#closeButton');
     
                 const disabledElements = new Set();
 
@@ -182,11 +104,12 @@
                 closeButton.addEventListener('click', () => {
                     document.body.removeChild(popup);
                     popup = null; // Reset the variable
+                    setTabStorage(session_id,"popup_enabled",false);
                 });
     
                 // populate with the stored fields
                 // Retrieve stored values from local storage
-                const storedIsChecked = await getTabStorage(session_id,"is_active");
+                const storedIsChecked = await getTabStorage(session_id,"is_active",false);
 
                 active.checked = storedIsChecked || false;
                 if (storedIsChecked) {
@@ -195,7 +118,7 @@
                     checkboxText.textContent = "Guide me only";
                 }
                 
-                const storedObjective = await getTabStorage(session_id,"objective");
+                const storedObjective = await getTabStorage(session_id,"objective","");
                 objectiveInput.value = storedObjective || "";
                 active.addEventListener("change", async function() {
                     if (active.checked) {
@@ -350,11 +273,99 @@
                     submitButton.removeAttribute("disabled");
                 
                 });
+        }
+        // Step 0: load imports
+        const src = chrome.runtime.getURL('./content.js');
+        const contentScript = await import(src);
+    
+        // Step 1: Create the round circle element
+        const circle = document.createElement('div');
+        circle.id = 'ai-assistance-circle';
+        // Add an image icon inside the circle
+        const icon = document.createElement('img');
+        icon.src = chrome.runtime.getURL('cricle.png'); // Update with the correct path to your 128x128 icon
+        icon.style.width = '100%';
+        icon.style.height = '100%';
+        
+        icon.style.clipPath = 'circle(50% at center)';
+        icon.style.webkitClipPath = 'circle(50% at center)';
+    
+        circle.appendChild(icon);
+    
+    
+        // Step 2: define draggable
+        // Additional code for making the circle draggable
+        let isDragging = false;
+        let offsetX, offsetY;
+    
+        // Function to handle the start of dragging
+        function startDrag(e) {
+            isDragging = true;
+            offsetX = e.clientX - circle.getBoundingClientRect().left;
+            offsetY = e.clientY - circle.getBoundingClientRect().top;
+        }
+    
+        // Function to handle the dragging movement
+        function dragMove(e) {
+            if (isDragging) {
+                const x = e.clientX - offsetX;
+                const y = e.clientY - offsetY;
+    
+                circle.style.left = `${x}px`;
+                circle.style.top = `${y}px`;
+            }
+        }
+    
+        // Function to handle the end of dragging
+        function endDrag() {
+            isDragging = false;
+        }
+    
+        // Add event listeners for dragging
+        circle.addEventListener('mousedown', startDrag);
+        document.addEventListener('mousemove', dragMove);
+        document.addEventListener('mouseup', endDrag);
+    
+    
+    
+        // Step 3: Apply CSS styles directly in JavaScript
+        circle.style.width = '50px';
+        circle.style.height = '50px';
+        circle.style.backgroundColor = '#3498db';
+        circle.style.borderRadius = '50%';
+        circle.style.position = 'fixed';
+        circle.style.bottom = '20px';
+        circle.style.right = '20px';
+        circle.style.display = 'flex';
+        circle.style.alignItems = 'center';
+        circle.style.justifyContent = 'center';
+        circle.style.cursor = 'pointer';
+        circle.style.color = 'white';
+        circle.style.fontWeight = 'bold';
+        circle.style.zIndex = '2147483646';
+
+        // Step 4: Add event listener to the circle element
+        circle.addEventListener('click', async function () {
+            if (popup) {
+                // If pop-up is already open, close it by removing it from the DOM
+                document.body.removeChild(popup);
+                popup = null; // Reset the variable
+                setTabStorage(session_id,"popup_enabled",false);
+            } else {
+                setTabStorage(session_id,"popup_enabled",true);
+                // Calculate position for the popupContent near the circle
+                await pop_menu()
             }
         });
-    
         // Step 5: Append the circle to the DOM
-        document.body.appendChild(circle);    
+        document.body.appendChild(circle);   
+
+        // Step 6: repload popup 
+        const popup_enabled = await getTabStorage(session_id,"popup_enabled",false);
+        if (popup_enabled){
+            await pop_menu()
+        }
+        
     }
     })();
     
