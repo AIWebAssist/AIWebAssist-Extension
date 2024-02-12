c107b4c7208f88ac533eb989300a3ad133af8d61
Sefi Erlich <erlichsefi@gmail.com>
2024-02-13 00:09:22 +0200

    .

--- extension/shared/act/page.js
+++ extension/shared/act/page.js
@@ -29,26 +29,21 @@ function go_back(){
 
 function move_to_url(url){
     if (!url.startsWith("http://") && !url.startsWith("https://")){
-        return "you should provide a url with http or https prefix."
+        return {
+            execution_status: false,
+            message: "you should provide a url with http or https prefix."
+        }
     }
 
     window.location.href = url;
 
-    setTimeout(() => {
-        if (window.location.href !== url){
-            return {
-                execution_status: false,
-                message: "url miss-match "+window.location.href
-            };
+    // any additional validation here require adding this into the next page onloading
+    return {
+        execution_status: true,
+        data: {
+            url: window.location.href
         }
-        return {
-            execution_status: true,
-            data:{
-                url:window.location.href
-            }
-        };
-    }, 2000); // Adjust timeout duration as needed
-
+    }
 }
 
 export {move_to_url,go_back,refresh};
