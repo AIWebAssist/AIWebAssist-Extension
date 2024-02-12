9b91f9bbe71f014589fd7570254f6b0587291dfc
Sefi Erlich <erlichsefi@gmail.com>
2024-02-12 21:37:34 +0200

    timeout

--- extension/shared/act/page.js
+++ extension/shared/act/page.js
@@ -27,26 +27,30 @@ function go_back(){
 
 }
 
-function move_to_url(url){
-    if (!url.startsWith("http://") && !url.startsWith("https://")){
-        return "you should provide a url with http or https prefix."
+function move_to_url(url) {
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        return "you should provide a url with http or https prefix.";
     }
 
     window.location.href = url;
 
-    if (window.location.href !== url){
-        return {
-            execution_status: false,
-            message: "url miss-match "+window.location.href
-        };
-    }
-    return {
-        execution_status: true,
-        data:{
-            url:window.location.href
-        }
-    };
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (window.location.href !== url) {
+                resolve({
+                    execution_status: false,
+                    message: "url miss-match " + window.location.href
+                });
+            } else {
+                resolve({
+                    execution_status: true,
+                    data: {
+                        url: window.location.href
+                    }
+                });
+            }
+        }, 2000); // Adjust timeout duration as needed
+    });
 }
 
 export {move_to_url,go_back,refresh};
